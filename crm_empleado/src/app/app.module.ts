83fc9d8fba92b7c0409c667eabec90ba82884313
Albert Altarriba <albertaltfer@gmail.com>
2022-07-15 19:06:57 +0200

    4. Components

--- crm_empleado/src/app/app.module.ts
+++ crm_empleado/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeesListComponent } from './components/employees-list/employees-list.component';
+import { FormComponent } from './components/form/form.component';
+import { EmployeeViewComponent } from './components/employee-view/employee-view.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesListComponent,
+    FormComponent,
+    EmployeeViewComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeCardComponent
   ],
   imports: [
     BrowserModule,
